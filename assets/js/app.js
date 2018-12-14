9cc32c4d68bc960d0b1c4d4b60fd5a86facda10c
Abram Wingle <abramwingle15@gmail.com>
2018-12-13 20:29:11 -0700

    got the gyazo api to display images on the site

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,6 @@
-
+    var gifDiv;
+    var personImage;
+    var results;
     var queryUrl = "https://api.gyazo.com/api/images?access_token=211603a169f957bac23aeb0e8f1efef90e0ca2568e426bfba70f452f1eb5b895"
 
     $.ajax({
@@ -6,5 +8,30 @@
         url: queryUrl
 
     }).then(function(response) {
-        console.log(response);
+        
+
+        results = response.data;
+
+
+        for (var i = 0; i < 10; i++) {
+
+            pictureDiv = $("<div>");
+        
+            personImage = $("<img class='newColumn col-md-12'>");
+            personImage.attr("src", response[i].url);
+        
+            pictureDiv.append(personImage);
+    
+            $(".newRow").prepend(pictureDiv);
+    
+        } 
+
+
     });
+
+
+
+
+    
+
+   
