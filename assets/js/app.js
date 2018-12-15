735f9074876cd358092118a652c76b0fa2cb78aa
Abram Wingle <abramwingle15@gmail.com>
2018-12-15 09:08:48 -0700

    changed the picture grid to flexbox

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,37 +1,35 @@
-    var gifDiv;
-    var personImage;
-    var results;
-    var queryUrl = "https://api.gyazo.com/api/images?access_token=211603a169f957bac23aeb0e8f1efef90e0ca2568e426bfba70f452f1eb5b895"
+var gifDiv;
+var personImage;
+var results;
+var queryUrl = "https://api.gyazo.com/api/images?access_token=211603a169f957bac23aeb0e8f1efef90e0ca2568e426bfba70f452f1eb5b895"
+var i;
+$.ajax({
+    method: "GET",
+    url: queryUrl
 
-    $.ajax({
-        method: "GET",
-        url: queryUrl
-
-    }).then(function(response) {
-        
-
-        results = response.data;
-
-
-        for (var i = 0; i < 10; i++) {
-
-            pictureDiv = $("<div>");
-        
-            personImage = $("<img class='newColumn col-md-12'>");
-            personImage.attr("src", response[i].url);
-        
-            pictureDiv.append(personImage);
-    
-            $(".newRow").prepend(pictureDiv);
-    
-        } 
+}).then(function (response) {
+    console.log(response);
+    results = response.data;
 
 
-    });
+    function createDiv() {
+        pictureDiv = $("<div>");
+
+        personImage = $("<img class='newColumn'>");
+        personImage.attr("src", response[i].url);
+
+        pictureDiv.append(personImage);
+
+       $("#pictureDisplay").prepend(pictureDiv);
+    }
 
 
+    for (i = 0; i < 9; i++) {
+
+        createDiv();
 
 
-    
+};
 
-   
+}
+)
